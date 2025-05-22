5b5c9e0c7a56e2bdc02c51af1f39866d9c3eb465
datmtp12345 <58815366+datmtp12345@users.noreply.github.com>
2025-05-22 16:39:36 +0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,2 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-app.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-analytics.js";
